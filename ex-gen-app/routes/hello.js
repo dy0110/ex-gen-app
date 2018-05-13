dfd2512792ccc399c29b6ac37eb5709b5bf6fa01
Daichi Yoshida <d.y.0110.barcelona@gmail.com>
2018-05-13 15:52:00 +0900

    フォーム送信の利用

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -2,17 +2,23 @@
 var express = require('express');
 var router = express.Router();
 
-//表示部分の作製
+//getリクエスト
 router.get('/',(req,res,next) => {
-    //クエリーパラメータ
-    var name = req.query.name;
-    var mail = req.query.mail;
-
     var data =  {
-        title : "Hello World !",
-        content : "あなたの名前は" + name + "<br>" + "あなたのメールアドレスは" + mail 
+        title : "Hello !",
+        content : "※入力して送信"
     };
     res.render('hello',data);
+});
+
+//postリクエスト
+router.post('/post',(req,res,next) => {
+    var msg = req.body.message;
+    var data = {
+        title : "Hello !",
+        content: "あなたは「" + msg +"」と送信しました"
+    };
+    res.render('hello' , data)
 });
 
 module.exports = router;
